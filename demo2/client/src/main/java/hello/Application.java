f551b6317ba63f267af92e5ca50100efd192d72d
steven <windknows@gmail.com>
2018-04-02 21:21:37 +0800

    added rest client

---
+++ demo2/client/src/main/java/hello/Application.java
@@ -0,0 +1,50 @@
+package hello;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.beans.factory.annotation.*;
+
+@SpringBootApplication
+public class Application {
+
+	private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+	public static void main(String args[]) {
+		SpringApplication.run(Application.class);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
+	@Value("${BACKEND_ENDPOINT}")
+	private String endpoint;
+
+	@Bean
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+		log.info(endpoint);
+		Person p = new Person();
+		p.setName("pname");
+		HttpEntity<Person> request = new HttpEntity<Person>(p);
+
+		return args -> {
+			Person person = restTemplate.postForObject(endpoint,
+					request, Person.class);
+			log.info(person.toString());
+		};
+
+		// return args -> {
+		// 	Person person = restTemplate.getForObject(
+		// 			endpoint, Person.class);
+		// 	log.info(person.toString());
+		// };
+	}
+}
