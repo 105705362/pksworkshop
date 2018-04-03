79f56dd815145a83e527025d178174fc49e1f8a2
steven <windknows@gmail.com>
2018-04-03 15:49:04 +0800

    added default value

--- demo2/client/src/main/java/hello/Application.java
+++ demo2/client/src/main/java/hello/Application.java
@@ -2,14 +2,14 @@ package hello;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.http.HttpEntity;
-import org.springframework.beans.factory.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class Application {
@@ -25,7 +25,7 @@ public class Application {
 		return builder.build();
 	}
 
-	@Value("${BACKEND_ENDPOINT}")
+	@Value("${BACKEND_ENDPOINT:http://localhost:5000}")
 	private String endpoint;
 
 	@Bean
@@ -36,15 +36,14 @@ public class Application {
 		HttpEntity<Person> request = new HttpEntity<Person>(p);
 
 		return args -> {
-			Person person = restTemplate.postForObject(endpoint,
-					request, Person.class);
+			Person person = restTemplate.postForObject(endpoint, request, Person.class);
 			log.info(person.toString());
 		};
 
 		// return args -> {
-		// 	Person person = restTemplate.getForObject(
-		// 			endpoint, Person.class);
-		// 	log.info(person.toString());
+		// Person person = restTemplate.getForObject(
+		// endpoint, Person.class);
+		// log.info(person.toString());
 		// };
 	}
 }
